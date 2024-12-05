430da532fffeefc860338e60264369938c07c954
sterino <egore.chip@gmail.com>
2024-12-06 02:21:08 +0500

    fix: migrations for influence added

---
+++ migrations/postgres/20241205212002_influence_schema.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS influencer (
+    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    address VARCHAR,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE influencer;
+-- +goose StatementEnd
