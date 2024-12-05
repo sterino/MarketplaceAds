6a558590f21128e743a27e2fde254cd495050591
sterino <egore.chip@gmail.com>
2024-12-06 02:59:01 +0500

    feat: code verification

--- internal/api/routes/route.go
+++ internal/api/routes/route.go
@@ -5,21 +5,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func InitRoutes(router *gin.RouterGroup, companyHandler *handler.CompanyHandler) {
+func InitRoutes(router *gin.RouterGroup, companyHandler *handler.CompanyHandler, influencerHandler *handler.InfluencerHandler) {
 	company := router.Group("/company")
 	{
 		//company.GET("/")
 		company.POST("/login", companyHandler.Login)
 		company.POST("/register", companyHandler.Register)
+		company.POST("/verify", companyHandler.VerifyEmail)
 		//company.PUT("/")
 		//company.GET("/")
 	}
 
-	//influencer := router.Group("/")
-	//{
-	//	influencer.GET("/")
-	//	influencer.POST("/")
-	//	influencer.PUT("/")
-	//}
+	influencer := router.Group("/")
+	{
+		influencer.POST("/login", influencerHandler.Login)
+		influencer.POST("/register", influencerHandler.Register)
+		//influencer.GET("/")
+		//influencer.POST("/")
+		//influencer.PUT("/")
+	}
 
 }
