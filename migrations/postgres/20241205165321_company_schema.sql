a8035e0aae8a74242b8bbbc2418a428869fae7c9
sterino <egore.chip@gmail.com>
2024-12-05 23:16:54 +0500

    fix: migrations

--- migrations/postgres/20241205165321_company_schema.sql
+++ migrations/postgres/20241205165321_company_schema.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS company (
-    id SERIAL PRIMARY KEY GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY GEN_RANDOM_UUID(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20),
-    address TEXT,
+    address VARCHAR,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
